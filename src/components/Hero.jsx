b2589385adf5aaf85908ed63d121eabca441b524
Tanay Tadas <91279967+tanaytadas16@users.noreply.github.com>
2023-04-10 19:39:26 -0500

    added scrollable framer, Hero Section done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,6 +24,21 @@ const Hero = () => {
                 </div>
             </div>
             <ComputersCanvas />
+            <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center ">
+                <a className="#about">
+                    <div className="w-[35px] h-[64px] border-4 border-secondary rounded-3xl flex justify-center items-start p-2">
+                        <motion.dev
+                            animate={{ y: [0, 24, 0] }}
+                            transition={{
+                                duration: 2,
+                                repeat: Infinity,
+                                repeatType: "loop",
+                            }}
+                            className="w-3 h-3 rounded-full bg-secondary mb-1"
+                        />
+                    </div>
+                </a>
+            </div>
         </section>
     );
 };
