400a8c85fa1a69038a69f37d3f1defae6e68cdba
Tanay Tadas <91279967+tanaytadas16@users.noreply.github.com>
2023-04-12 16:47:51 -0500

    added HOC Section Wrapper and About section is done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,69 @@
 import React from "react";
-import Tilt from "react-parallax-tilt";
+import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
-// import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
+const fadeIn = (direction, type, delay, duration) => {
+    return {
+        hidden: {
+            x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
+            y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+            opacity: 0,
+        },
+        show: {
+            x: 0,
+            y: 0,
+            opacity: 1,
+            transition: {
+                type: type,
+                delay: delay,
+                duration: duration,
+                ease: "easeOut",
+            },
+        },
+    };
+};
 const ServiceCard = ({ index, title, icon }) => {
-    return <p>{title}</p>;
+    return (
+        <Tilt className="xs:w-[250px] w-full">
+            <motion.div
+                variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+                className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+            >
+                <div
+                    options={{ max: 45, scale: 1, speed: 450 }}
+                    className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex flex-col justify-center items-center"
+                >
+                    <img
+                        src={icon}
+                        alt={title}
+                        className="w-16 h-16 object-contain"
+                    />
+                    <h3 className=" text-white text-[20px] font-bold text-center">
+                        {title}
+                    </h3>
+                </div>
+            </motion.div>
+            <p>{title}</p>
+        </Tilt>
+    );
 };
 
 const About = () => {
     return (
         <>
-            <motion.div>
+            <motion.div variants={textVariant()}>
                 <p className={styles.sectionSubText}>Introduction</p>
                 <h2 className={styles.sectionHeadText}>Overview.</h2>
             </motion.div>
             <motion.p
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.5 }}
+                variants={fadeIn("", "", 0.1, 1)}
+                // initial={{ opacity: 0, scale: 0.5 }}
+                // animate={{ opacity: 1, scale: 1 }}
+                // transition={{ duration: 0.5 }}
                 className="mt-4 text-secondary text-[17px] max-w-3xl leading-9"
             >
                 I'm a skilled software developer with experience in TypeScript
@@ -42,4 +86,4 @@ const About = () => {
     );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
